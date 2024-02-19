0415b0533da952cd9413f3d3ea038d18c23eb5a9
kureta <skureta@gmail.com>
2024-02-19 20:19:13 +0300

    Added missing docstrings and types

--- ghost_tail/main.py
+++ ghost_tail/main.py
@@ -1,3 +1,4 @@
+"""Loads midi files from a directory and extracts piano tracks from them."""
 from __future__ import annotations
 
 import os
@@ -35,8 +36,8 @@ def _log_formatter(record: loguru.Record) -> str:
     """Log message formatter"""
     color = _get_record_color(record)
     return (
-            f"[not bold green]{record['time']:YYYY/MM/DD HH:mm:ss}[/not bold green] | "
-            + f"{record['level'].icon} - [{color}]{{message}}[/{color}]"
+        f"[not bold green]{record['time']:YYYY/MM/DD HH:mm:ss}[/not bold green] | "
+        + f"{record['level'].icon} - [{color}]{{message}}[/{color}]"
     )
 
 
@@ -49,14 +50,23 @@ console = Console(color_system="truecolor", stderr=True)
 
 
 def track_has_note_events(track: mido.MidiTrack) -> bool:
+    """Some tracks have no note events. We need to filter them out."""
     return any(message.type == "note_on" for message in track)
 
 
 def track_has_program_change(track: mido.MidiTrack) -> bool:
+    """Program change messages are used to change the instrument.
+    If a track has no program change, it is assumed to be a piano track.
+    """
     return any(message.type == "program_change" for message in track)
 
 
-def get_first_program_change_from_track(track: mido.MidiTrack) -> Union[mido.Message, None]:
+def get_first_program_change_from_track(
+    track: mido.MidiTrack,
+) -> Union[mido.Message, None]:
+    """Get the first program change message from a track. This is used to determine the instrument.
+    Currently, we only look at the first program change message.
+    """
     for message in track:
         if message.type == "program_change":
             return message.program
@@ -64,7 +74,11 @@ def get_first_program_change_from_track(track: mido.MidiTrack) -> Union[mido.Mes
     return None
 
 
-def get_track_with_piano_in_name(mid: List[mido.MidiTrack]) -> Union[mido.MidiTrack, None]:
+# TODO: handle multiple tracks with piano in name, they are probably 2 hands of the same piano.
+def get_track_with_piano_in_name(
+    mid: List[mido.MidiTrack],
+) -> Union[mido.MidiTrack, None]:
+    """See if there is a track with piano in its name. If there is only one, return it."""
     tracks = [track for track in mid if "piano" in track.name.strip().lower()]
 
     if len(tracks) == 1:
@@ -78,12 +92,15 @@ def get_track_with_piano_in_name(mid: List[mido.MidiTrack]) -> Union[mido.MidiTr
 
 
 def get_piano_track_from_mid(mid: mido.MidiFile) -> Union[mido.MidiTrack, None]:
+    """Apply a series of heuristics to find the piano track in a midi file."""
     # get all tracks with note events
     tracks = [track for track in mid.tracks if track_has_note_events(track)]
 
     # has only one track
     if len(tracks) == 1:
-        logger.trace("Found only one track with note events. Assuming it is the piano track.")
+        logger.trace(
+            "Found only one track with note events. Assuming it is the piano track."
+        )
         return tracks[0]
 
     # has piano in name
@@ -117,6 +134,8 @@ def get_piano_track_from_mid(mid: mido.MidiFile) -> Union[mido.MidiTrack, None]:
 
 
 class Status(Enum):
+    """Status of midi file processing."""
+
     VALID = 1
     NO_PIANO = 2
     CORRUPTED = 3
@@ -124,12 +143,15 @@ class Status(Enum):
 
 @dataclass
 class Result:
+    """Result of processing a midi file."""
+
     filename: Path
     track: mido.MidiTrack
     status: Status
 
 
 def get_piano_track_from_file(full_path: Path) -> Result:
+    """Load a midi file and extract the piano track from it."""
     filename = full_path.name
     # load midi file using mido
     try:
@@ -147,6 +169,7 @@ def get_piano_track_from_file(full_path: Path) -> Result:
 
 
 def get_piano_tracks_from_dir(midi_dir: Path) -> List[mido.MidiTrack]:
+    """Load all midi files from a directory and extract the piano tracks from them."""
     # iterate through all midi files in directory
     files = list(midi_dir.glob("*.mid")) + list(midi_dir.glob("*.MID"))
     logger.info(f"Found {len(files)} midi files in {midi_dir}. Processing...")
@@ -180,7 +203,9 @@ def get_piano_tracks_from_dir(midi_dir: Path) -> List[mido.MidiTrack]:
 # TODO: convert tracks to data format (not specified yet)
 # TODO: add pre-commit hooks
 # TODO: add nbstripout
-def main(args):
+# TODO: make into a proper CLI
+def main(args: List[str]) -> None:
+    """Main function. Entry point of the program."""
     if len(args) > 1:
         print("Usage: python main.py <path to midi directory>")
         exit(1)
@@ -199,7 +224,7 @@ if __name__ == "__main__":
     import sys
 
     load_dotenv()
-    log_level = os.getenv("LOG_LEVEL", "INFO")
+    log_level = os.getenv("LOG_LEVEL", "TRACE")
 
     logger.remove()
     logger.add(
@@ -208,11 +233,13 @@ if __name__ == "__main__":
         format=_log_formatter,
         colorize=True,
     )
+
+    # Systemd journal logging does not support TRACE level
+    # Default to INFO
     logger.add(
         JournaldLogHandler(identifier="Ghost Tail"),
-        level=log_level,
+        level="INFO",
         format=_journald_formatter,
-        colorize=False,
     )
 
     main(sys.argv[1:])
